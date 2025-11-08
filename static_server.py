60d511e99b9c57a9d2269cbbd0eb909f9befee8f
Ernesto Cristian Merino <ernestomerinocc@gmail.com>
2025-11-08 00:07:42 -0300

    Corrijo static_server.py para servir archivos estáticos correctamente

--- static_server.py
+++ static_server.py
@@ -7,13 +7,4 @@ def serve_icon(filename):
     return send_from_directory('icons', filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)from flask import Flask, send_from_directory
-
-app = Flask(__name__)
-
-@app.route('/icons/<path:filename>')
-def serve_icon(filename):
-    return send_from_directory('icons', filename)
-
-if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
